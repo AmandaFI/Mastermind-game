bafcebc6402d21594f2e78580c888ade2491a5c9
DanielG-s <53453411+DanielG-s@users.noreply.github.com>
2022-06-18 16:44:51 -0300

    Modificação código cliente
    Adição de tratativas na conexão inicial e na desconexão final do cliente ao servidor.

--- cliente.c
+++ cliente.c
@@ -9,6 +9,11 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <string.h>
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 
 
 int main(){
@@ -40,13 +45,52 @@ int main(){
 	//Array auxiliar
 	char aux_str[600];
 
+  //Variável auxiliar para verificação de IP
+  int contadorponto = 0;
+
+  //Variável auxiliar para verificação de tentativas de IP
+  int tentativas = 0;
+
   printf("Digite o IP de onde deseja conectar:\n");
   scanf("%s", addr_string);
+  
+  //Caso o formato de IP digitado não seja válido, ele terá 5 tentativas de digitá-lo novamente no formato correto (IPV4)
+  while (tentativas < 5){ 
+    
+    for (int i=0;i <= strlen(addr_string);i++){  
+      if (addr_string[i] == '.' || addr_string[i] == ':' ){
+        contadorponto++;
+      }
+    }
+
+    if (contadorponto != 3){ 
+      printf("\nIP necessita estar no formato IPV4!\n");
+      printf("Digite o IP de onde deseja conectar:\n");
+      scanf("%s", addr_string);
+      contadorponto=0;
+      tentativas++;
+    }
+    else{
+      break;
+    }
+  }
+
+  //caso o usuário exceda as tentativas, o cliente será encerrado
+  if (tentativas == 5)
+  {
+    printf("\nNúmero máximo de tentativas excedido, não foi possível criar o socket\n");
+    exit(1);
+  }
+  
+ 
+  
+
+
   __fpurge(stdin); //limpar buffer de leitura
 
   //Converte o IP string para IP int
   inet_pton(AF_INET, addr_string, &addr_int);
-  printf("\n%d\n",addr_int.s_addr);
+
 
   printf("Digite a porta na qual deseja conectar:\n");
   scanf("%d", &porta);
@@ -61,8 +105,6 @@ int main(){
     }
   else{
       printf("Criação do socket com sucesso.\n");
-    // printf("\n%d", socket_id);
-    // close(socket_id);
     }
 
   //Zera struct com as informações de conexão (limpar lixo de memória)
@@ -84,7 +126,7 @@ int main(){
     }
   else{
       printf("Conexão efetuada com sucesso.\n");
-      //close(socket_id);
+
     }
 
   while (fim == 0) {
@@ -95,32 +137,45 @@ int main(){
       printf("Erro ao receber mensagem. A conexão será encerrada");
       close(socket_id);
       exit(1);
-    } else if (recv_str[0]=='.') {
+    } else if (recv_str[0]=='.') { //Se o primeiro caractere da resposta do servidor for "." será removido da string e a conexão será encerrada
       memmove(recv_str,recv_str+1,strlen(recv_str));
-			printf("%s\n",recv_str);
+			printf("\n%s\n",recv_str);
       close(socket_id);
       exit(1);
-    } else if (recv_str[0]=='-'){
+    } else if (recv_str[0]=='-'){ //Se o primeiro caractere da resposta do servidor for "-" será removido da string
 			memmove(recv_str,recv_str+1,strlen(recv_str));
-			printf("%s\n",recv_str);
+			printf("\n%s\n",recv_str);
 		} else {
-			printf("%s\n",recv_str);
-		}
+  
+      if(strlen(recv_str) <= 4){ // Verifica se o servidor retornou algo além dos erros e acertos atuais
 
-    if (recv_str[0]=='.') {
+        printf(ANSI_COLOR_GREEN"%c Número(s) correto(s) nas posições corretas \n"ANSI_COLOR_RESET,recv_str[0]);
+        printf(ANSI_COLOR_YELLOW"%c Número(s) correto(s) nas posições incorretas \n"ANSI_COLOR_RESET, recv_str[1]);
+        printf(ANSI_COLOR_RED"%c Número(s) incorreto(s)"ANSI_COLOR_RESET"\n",recv_str[2]);
+
+      }
+
+      else{ // Caso esgotar as tentativas, o servidor irá retornar os erros e acertos, a mensagem final e a conexão será encerrada
+
+        printf(ANSI_COLOR_GREEN"%c Número(s) corretos nas posições corretas \n"ANSI_COLOR_RESET,recv_str[0]);
+        printf(ANSI_COLOR_YELLOW"%c Número(s) corretos nas posições incorretas \n"ANSI_COLOR_RESET, recv_str[1]);
+        printf(ANSI_COLOR_RED"%c Número(s) incorretos"ANSI_COLOR_RESET"\n\n",recv_str[2]);
+        
+        memmove(recv_str,recv_str+5,strlen(recv_str));
+
+        printf("%s\n",recv_str);
         close(socket_id);
         exit(1);
-    }
+
+      }
+
+      
+		}
+
 
     scanf("%s",send_str);
     __fpurge(stdin);
-    //printf("%s\n",send_str);
 
-   //if (send_str == 'quit') {
-   //  printf("Até mais :)");
-   //  close(socket_id);
-   //  exit(1);
-   //}
 
     if ((aux = send(socket_id,send_str,sizeof(send_str),0)) == -1){
       printf("Erro ao enviar mensagem. A conexão será encerrada");
